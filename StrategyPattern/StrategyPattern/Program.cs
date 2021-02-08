783ca03661fffbf636a75e1fdfa72be792559358
Vecopotryx <Repsejk@gmail.com>
2021-02-08 14:22:36 +0100

    Added RunStrategy

--- StrategyPattern/StrategyPattern/Program.cs
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -6,10 +6,20 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
-            WalkStrategy walker = new WalkStrategy();
-            walker.Move(1, "left");
-
-
+            Console.WriteLine("Would you like to run?");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    RunStrategy runner = new RunStrategy();
+                    runner.Move(3, "right");
+                    break;
+                case "2":
+                    WalkStrategy walker = new WalkStrategy();
+                    walker.Move(1, "left");
+                    break;
+            }
         }
     }
 }
---
+++ StrategyPattern/StrategyPattern/RunStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern
+{
+    class RunStrategy : IMovementStrategy
+    {
+        public void Move(double velocity, string direction)
+        {
+            Console.WriteLine("Running towards {0} at {1} m/s", direction, velocity);
+        }
+    }
+}
