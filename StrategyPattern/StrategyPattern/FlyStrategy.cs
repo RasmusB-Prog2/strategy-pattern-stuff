63b42519707c5430e49ca27bf56e2490df80d011
Vecopotryx <Repsejk@gmail.com>
2021-02-08 14:26:22 +0100

    Added FlyStrategy

---
+++ StrategyPattern/StrategyPattern/FlyStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern
+{
+    class FlyStrategy
+    {
+        public void Move(double velocity, string direction)
+        {
+            Console.WriteLine("You somehow acquired wings and are now flying towards {0} at {1} m/s", direction, velocity);
+        }
+    }
+}
--- StrategyPattern/StrategyPattern/Program.cs
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -6,9 +6,10 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Would you like to run?");
-            Console.WriteLine("1. Yes");
-            Console.WriteLine("2. No");
+            Console.WriteLine("You need to go somewhere else, what do you do?");
+            Console.WriteLine("1. Run");
+            Console.WriteLine("2. Walk");
+            Console.WriteLine("3. Fly");
             switch (Console.ReadLine())
             {
                 case "1":
@@ -18,6 +19,10 @@ namespace StrategyPattern
                 case "2":
                     WalkStrategy walker = new WalkStrategy();
                     walker.Move(1, "left");
+                    break;
+                case "3":
+                    FlyStrategy flyer = new FlyStrategy();
+                    flyer.Move(120, "west");
                     break;
             }
         }
